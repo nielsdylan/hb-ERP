055384ec85219358e70bfbbd1ecdbb1613342271
OKCNiels <programador01@okcomputer.com.pe>
2024-01-30 12:14:41 -0500

    cambios

--- resources/views/components/academico/aulas/report/asistencia.blade.php
+++ resources/views/components/academico/aulas/report/asistencia.blade.php
@@ -13,7 +13,8 @@
     @endphp
     @foreach ($imagenes as $value)
         @if ($value->path_dni)
-            <div style="margin: 25px !important;"><img src="{{public_path().'/'.$value->path_dni}}" width="100%" ></div>
+            {{-- <div style="margin: 25px !important;"><img src="{{public_path().'/'.$value->path_dni}}" width="100%" ></div> --}}
+            <div style="margin: 25px !important;"><img src="{{{{ asset(''.$value->path_dni) }}}}" width="100%" ></div>
         @endif
     @endforeach
 </body>
